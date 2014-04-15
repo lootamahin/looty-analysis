bf47167999c65ee7e596bcf8b872b70bd524a0f6
lootamahin <jagdevbrar@gmail.com>
2014-04-15 17:28:30 -0400

    Minor Changes To Parsing
    Still encountering some errors... with the last block of data for
    GAMETYPE: URF

--- looty_analysis/summoners/parsestats.py
+++ looty_analysis/summoners/parsestats.py
@@ -44,30 +44,36 @@ class ParseStats:
                     dongturdchunk = dongturdchunk.replace("|", "")
                     dongturdchunk = dongturdchunk.replace("aggregatedStats:", "")
                     
-                    eachstatset = dongturdchunk.split(';')
-                    aggstats_block = aggstats_block + "<b>AGGREGATED STATS:</b><br>"
-                    
-                    for eachstat in eachstatset:
-                        tinystats = eachstat.partition(':')
-                        aggstats_block = aggstats_block + "<b>OMG? " + tinystats[0] + ":</b> <i>" + tinystats[2] + "</i><br>"
+                    if ":" in dongturdchunk:
+                        eachstatset = dongturdchunk.split(';')
+                        aggstats_block = aggstats_block + "<br><b><u>AGGREGATED STATS:</u></b><br>\n"
+                        
+                        for eachstat in eachstatset:
+                            tinystats = eachstat.partition(':')
+                            aggstats_block = aggstats_block + "<b>AS: " + tinystats[0] + ":</b> <i>" + tinystats[2] + "</i><br>\n"
+
+                    else:
+                        aggstats_block = "<br>NO AGG STATS HERE!!!"
                         
                 else:
                     dongturdchunkdrop = dongturdchunk.partition(':') #EX: modifyDate:1397308828000
                     
                     if "playerStatSummaryType" in dongturdchunkdrop[0]:
-                        gametype_header = gametype_header + "<br><b><u>GAME TYPE:</u> </b>" + dongturdchunkdrop[2] + "<br>"
+                        gametype_header = gametype_header + "<br><b><u>GAME TYPE:</u></b> " + dongturdchunkdrop[2] + "<br>\n"
                         
                     else:
-                        allotherstats_block = allotherstats_block + "<b>WTF? " + dongturdchunkdrop[0] + ":</b> " + dongturdchunkdrop[2] + "<br>"
+                        allotherstats_block = allotherstats_block + "<b>" + dongturdchunkdrop[0] + ":</b> " + dongturdchunkdrop[2] + "<br>\n"
 
-            statsdata_all += gametype_header + allotherstats_block + aggstats_block + "<br>&nbsp;<br><b>" + str(i) + "</b><br>&nbsp;<br>"
+            #statsdata_all += gametype_header + allotherstats_block + "<font size=\"10\">" + aggstats_block + "</font><br>&nbsp;<br><b>" + str(i) + "</b><br>&nbsp;<br>"
+            statsdata_all += gametype_header + "\n" + allotherstats_block + "\n<font size=\"2\">" + aggstats_block + "</font>\n<br>&nbsp;<br>\n\n"
             gametype_header = ""
             allotherstats_block = ""
             aggstats_block = ""
             i += 1
              
         #self.stats = "<html>\n<body>\n<font face=\"Arial\">\n" + self.stats + "\n</font>\n</body>\n</html>\n\n\n\n<br>&nbsp;<br>\n\n\n\n" + JAGTEST + "\n\n\n\n<br>&nbsp;<br>\n\n\n\n<font face\"Arial\">" + purtyhtml
-        self.stats = "<html>\n<body>\n<font face=\"Arial\">\n" + JAGTEST + "\n\n\n\n<br>&nbsp;<br>\n\n\n\n<font face\"Arial\">" + statsdata_all
+        #self.stats = "<html>\n<body>\n<font face=\"Arial\">\n" + self.stats + "\n\n\n\n<br>&nbsp;<br>\n\n\n\n<font face\"Arial\">" + JAGTEST + "\n\n\n\n<br>&nbsp;<br>\n\n\n\n<font face\"Arial\">" + statsdata_all
+        self.stats = "<html>\n<body>\n<font face=\"Arial\">\n" + statsdata_all + "</font>\n</body>\n</html>"
         self.stats = self.stats.replace("|", "")
         response = self.stats
         
